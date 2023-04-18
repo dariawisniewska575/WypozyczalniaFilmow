3eda655583dd4441b2e893382c0259d0396b09cf
Łukasz <lukaszwluka@interia.pl>
2023-04-18 19:21:36 +0200

    add default movies inserts

---
+++ MovieRentApp/Movies.sql
@@ -0,0 +1,23 @@
+﻿INSERT INTO Movies (Title, ReleaseYear, Description, IsAvaiable, Category)
+VALUES ('Gwiezdne wojny: Nowa nadzieja', 1977, 'Film opowiada historię młodego farmaceuty, który wstępuje do ruchu oporu przeciwko Imperium Galaktycznemu', 1, 0);
+
+INSERT INTO Movies (Title, ReleaseYear, Description, IsAvaiable, Category)
+VALUES ('Indiana Jones i Świątynia Zagłady', 1984, 'Film opowiada o przygodach profesora Indiany Jonesa w poszukiwaniu arcydzieła religijnego', 1, 1);
+
+INSERT INTO Movies (Title, ReleaseYear, Description, IsAvaiable, Category)
+VALUES ('Służące', 2018, 'Film opowiada o życiu trzech kobiet pracujących jako służące w elitarnych domach w Meksyku', 0, 3);
+
+INSERT INTO Movies (Title, ReleaseYear, Description, IsAvaiable, Category)
+VALUES ('Król Lew', 1994, 'Film opowiada historię młodego lwa Simby, który musi zmierzyć się z wyzwaniami dorosłego życia i odzyskać tron swojego ojca', 1, 4);
+
+INSERT INTO Movies (Title, ReleaseYear, Description, IsAvaiable, Category)
+VALUES ('Forrest Gump', 1994, 'Film opowiada o życiu Forresta Gumpa, mężczyzny o niskim ilorazie inteligencji, który jednak odnosi sukcesy w różnych dziedzinach życia', 1, 2);
+
+INSERT INTO Movies (Title, ReleaseYear, Description, IsAvaiable, Category)
+VALUES ('Nietykalni', 2011, 'Film opowiada o relacji między bogatym białym arystokratą, a czarnoskórym bezrobotnym z przedmieść Paryża', 1, 6);
+
+INSERT INTO Movies (Title, ReleaseYear, Description, IsAvaiable, Category)
+VALUES ('Titanic', 1997, 'Film opowiada historię miłości młodej pary na pokładzie legendarnego transatlantyka', 1, 5);
+
+INSERT INTO Movies (Title, ReleaseYear, Description, IsAvaiable, Category)
+VALUES ('Avengers: Wojna bez granic', 2018, 'Film opowiada o konfrontacji grupy superbohaterów z kosmicznym tyranoza Thanosem', 1, 0);
